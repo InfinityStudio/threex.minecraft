82ef62464bf238398637f1e7bc24152cf039c459
ci010 <cijhn@hotmail.com>
2017-09-02 09:59:00 -0700

    update

--- test/bundle.js
+++ test/bundle.js
@@ -46625,7 +46625,6 @@ module.exports = function remap() {
 
     let geometry
     geometry = this.head.geometry;
-    console.log(geometry)
     this.mapUv(geometry, 0, 16, 24 + offset, 24, 16 + offset)	// left
     this.mapUv(geometry, 1, 0, 24 + offset, 8, 16 + offset)	// right
     this.mapUv(geometry, 2, 8, 32 + offset, 16, 24 + offset)	// top
@@ -46634,7 +46633,6 @@ module.exports = function remap() {
     this.mapUv(geometry, 5, 24, 24 + offset, 32, 16 + offset)	// back
 
     geometry = this.helmet.geometry
-    console.log(geometry)
     this.mapUv(geometry, 0, 48, 24 + offset, 56, 16 + offset)	// left
     this.mapUv(geometry, 1, 32, 24 + offset, 40, 16 + offset)	// right
     this.mapUv(geometry, 2, 40, 32 + offset, 48, 24 + offset)	// top
@@ -46643,7 +46641,6 @@ module.exports = function remap() {
     this.mapUv(geometry, 5, 56, 24 + offset, 64, 16 + offset)	// back
 
     geometry = this.body.geometry
-    console.log(geometry)
     this.mapUv(geometry, 0, 28, 12 + offset, 32, 0 + offset)	// left
     this.mapUv(geometry, 1, 16, 12 + offset, 20, 0 + offset)	// right
     this.mapUv(geometry, 2, 20, 16 + offset, 28, 12 + offset)	// top
@@ -46663,7 +46660,6 @@ module.exports = function remap() {
     }
 
     geometry = this.armR.geometry;
-    console.log(geometry)
     this.mapUv(geometry, 0, 48, 12 + offset, 52, 0 + offset)	// right
     this.mapUv(geometry, 1, 40, 12 + offset, 44, 0 + offset)	// left
     this.mapUv(geometry, 2, 44, 16 + offset, 48, 12 + offset)	// top
@@ -46672,7 +46668,6 @@ module.exports = function remap() {
     this.mapUv(geometry, 5, 52, 12 + offset, 56, 0 + offset)	// back
 
     geometry = this.armL.geometry;
-    console.log(geometry)
     this.mapUv(geometry, 0, 44 + offset, 12, 40, 0 + offset)	// right
     this.mapUv(geometry, 1, 52 + offset, 12, 48, 0 + offset)	// left
     this.mapUv(geometry, 2, 44 + offset, 16, 48, 12 + offset)	// top
@@ -46681,7 +46676,6 @@ module.exports = function remap() {
     this.mapUv(geometry, 5, 56 + offset, 12, 52, 0 + offset)	// back
 
     geometry = this.legR.geometry;
-    console.log(geometry)
     this.mapUv(geometry, 0, 8 + offset, 12, 12, 0 + offset)	// right
     this.mapUv(geometry, 1, 0 + offset, 12, 4, 0 + offset)	// left
     this.mapUv(geometry, 2, 4 + offset, 16, 8, 12 + offset)	// top
@@ -46690,7 +46684,6 @@ module.exports = function remap() {
     this.mapUv(geometry, 5, 12 + offset, 12, 16, 0 + offset)	// back
 
     geometry = this.legL.geometry;
-    console.log(geometry)
     this.mapUv(geometry, 0, 4, 12 + offset, 0, 0 + offset)	// left
     this.mapUv(geometry, 1, 12, 12 + offset, 8, 0 + offset)	// right
     this.mapUv(geometry, 2, 8, 16 + offset, 4, 12 + offset)	// top
